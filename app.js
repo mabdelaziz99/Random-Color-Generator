0c3758ed7c697b2bcfee6b7724304f71648f5bf5
Mahmoud Abdelaziz <mabdelaziz25899@gmail.com>
2024-11-04 21:24:59 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+const button = document.querySelector('#btn1')
+const text1 = document.querySelector('#hi')
+const lastDiv = document.querySelector('#sp')
+const ParentDiv = document.querySelector('#parent')
+
+function rewrite(){
+const x1 = (Math.floor(Math.random()*255))
+const x2 = (Math.floor(Math.random()*255))
+const x3 = (Math.floor(Math.random()*255))
+const colorText = `rgb(${x1},${x2},${x3})`
+text1.innerText = colorText
+document.body.style.backgroundColor = `rgb(${x1},${x2},${x3})`
+console.log('clicked')
+}
+function pop(){
+    const newDiv = document.createElement('div')
+    const newText = document.createElement('h1')
+newText.innerText = "YOU'RE A DUMBASS"
+ParentDiv.appendChild(newDiv);
+newDiv.appendChild(newText)
+}
+
+button.addEventListener('click', rewrite)
+button.addEventListener('click', pop)
